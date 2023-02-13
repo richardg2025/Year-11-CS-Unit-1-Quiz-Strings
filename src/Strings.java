2e6691aff2b28d3466de9ace180eba4594b2b24f
Adam <adam.ewing.nz@gmail.com>
2023-02-13 08:20:36 +0800

    Added commented numbers for questions.

--- src/Strings.java
+++ src/Strings.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Strings {
+public class Strings {
+
+    public static void main(String[] args) {
+
+        // 1.
+
+        // 2.
+
+        // 3.
+
+        // 4.
+
+        // 5.
+
+        // 6.
+
+        // 7.
+
+        // 8.
+
+        // 9.
+
+        // 10.
+
+    }
+
 }
